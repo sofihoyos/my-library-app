720ccb042d9f3afa8f4cc758237ac2250fbd9e1a
sofihoyos <SenaManana@ARMDFPCCIFDS013.FormacionSena.RED>
2024-06-13 07:12:29 -0500

    tipado primitivo

---
+++ src/variiables-types.ts
@@ -0,0 +1,21 @@
+let bookTitle:string="TypeScript Basics";
+const libraryName:string="My Library";
+let numberOfBooks: number=250;
+let isLibratyOpen:boolean=true;
+
+let titulo:string;
+let autor:string;
+let genero:string;
+let idioma:string;
+let precio:number;
+let formato:string;
+let isbn:number;
+let descripcion:string;
+let estado:string;
+let ubicacion:string;
+let fecha_publicacion:Date;
+let editorial:string;
+let paginas:number;
+let dimensiones:string;
+let peso:string;
+let disponible:string;
